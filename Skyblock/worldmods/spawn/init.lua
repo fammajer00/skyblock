bc8ae5d454545dc6affa1b6c846d2897a1f268cf
4Evergreen4 <eljohnson@frontier.com>
2013-06-04 21:53:59 -0400

    Added 2 blocks of sand, put 1 cactus in the chest.

--- Skyblock/worldmods/spawn/init.lua
+++ Skyblock/worldmods/spawn/init.lua
@@ -2,3 +2,8 @@ minetest.register_on_respawnplayer(function(player)
     player:setpos({x=2.0, y=1.8, z=0.0})
     return true
 end)
+
+minetest.register_on_newplayer(function(player)
+    player:setpos({x=2.0, y=1.8, z=0.0})
+    return true
+end)
